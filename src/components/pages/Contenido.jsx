036f9d4ec6cd039f93496efc422f1049222a4fd8
alfred <jhalfred96@gmail.com>
2023-05-21 17:15:01 -0500

    se corrigen el ancho de la iagen del contenido

--- src/components/pages/Contenido.jsx
+++ src/components/pages/Contenido.jsx
@@ -12,7 +12,7 @@ const Contenido = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-[100vh]'>
+            <div className='md:w-[100vh] '>
                 <div className='flex flex-col items-center p-4'>
                     <img className='img' src={mapaNA} alt="" width="100%" height='100%' />
                 </div>
